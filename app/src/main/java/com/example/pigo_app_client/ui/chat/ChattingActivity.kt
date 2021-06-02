add4cb3a630a18e1d34fff9f37f7f2104c85ed86
류호성 <52982845+Hoseong-Ryu@users.noreply.github.com>
2021-06-02 13:52:59 +0900

    CreateChattingActivity

---
+++ app/src/main/java/com/example/pigo_app_client/ui/chat/ChattingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pigo_app_client.ui.chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pigo_app_client.R
+
+class ChattingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chatting)
+    }
+}
