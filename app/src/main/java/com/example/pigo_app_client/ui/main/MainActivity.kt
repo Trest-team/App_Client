98cc06a135cdc0e46f89f141102cf1f7fe4d9508
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-06-07 15:14:02 +0900

    MainActivity 클래스 위치 변경

--- app/src/main/java/com/example/pigo_app_client/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/pigo_app_client/ui/main/MainActivity.kt
@@ -1,11 +1,23 @@
-package com.example.pigo_app_client
+package com.example.pigo_app_client.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pigo_app_client.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initBottomNav()
+
     }
+
+    private fun initBottomNav() {
+        
+
+    }
+
+
 }
