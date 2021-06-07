234119607f429e39b0980b7294bc815b5fdf8e55
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-06-07 16:52:13 +0900

    feat : MainActivity bottom navigation에 연결할 Fragment 생성

---
+++ app/src/main/java/com/example/pigo_app_client/ui/main/fragment/CounselFragment.kt
@@ -0,0 +1,18 @@
+package com.example.pigo_app_client.ui.main.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.pigo_app_client.R
+
+
+class CounselFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_counsel, container, false)
+    }
+
+}
